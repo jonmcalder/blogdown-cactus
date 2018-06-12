8418737ac46a1e4a81b68c48b1a5e1d87901e1fc
Lorenz Walthert <lorwal@me.com>
2018-06-12 13:15:35 +0200

    another trigger for automated netlify CI build via GitHub (which we don't care, but we'd like it not to fail).

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ if [ $(git symbolic-ref -q HEAD) = "refs/heads/master" ]
 then
 	  echo "you are on master, deploying production."
 	  netlify deploy -t $NETLIFYKEY --path public
-	  
 else 
     echo "you are not on master, deploying preview."
     netlify deploy -t $NETLIFYKEY --draft --path public
