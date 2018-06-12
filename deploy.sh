2a1cde8b30d407c999742c8a8155a09d57f4cf23
Lorenz Walthert <lorwal@me.com>
2018-06-12 12:51:02 +0200

    use daft mode an path argument

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@ echo "$PATH"
 if [ $(git symbolic-ref -q HEAD) = "refs/heads/master" ]
 then
 	  echo "you are on master, deploying production."
-	  netlify deploy -t $NETLIFYKEY
+	  netlify deploy -t $NETLIFYKEY --path public
 	  
 else 
     echo "you are not on master, deploying preview."
-    cd public
-    netlify deploy -t $NETLIFYKEY
+    netlify deploy -t $NETLIFYKEY --draft --path public
 fi
