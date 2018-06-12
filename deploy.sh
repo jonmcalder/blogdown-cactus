511f9c12f25d092cd5d11a78dc587e6bdd7ee52a
lorenzwalthert <lorenz.walthert@icloud.com>
2018-06-12 14:41:36 +0200

    don't set path as argument but use .netlify explicitly

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ echo "$PATH"
 if [ $(git symbolic-ref -q HEAD) = "refs/heads/master" ]
 then
 	  echo "you are on master, deploying production."
-	  netlify deploy -t $NETLIFYKEY --path public
+	  netlify deploy -t $NETLIFYKEY
 else 
     echo "you are not on master, deploying preview."
-    netlify deploy -t $NETLIFYKEY --draft --path public
+    netlify deploy -t $NETLIFYKEY --draft
 fi
