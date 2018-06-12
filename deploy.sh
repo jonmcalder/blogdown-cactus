677a8679e64aa9774ce905cd14544fe275530eea
Lorenz Walthert <lorwal@me.com>
2018-06-12 11:35:32 +0200

    add custom deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "$PATH"
+if [ $(git symbolic-ref -q HEAD) = "refs/heads/master" ]
+then
+	  echo "you are on master, deploying production."
+	  netlify deploy -t $NETLIFYKEY
+	  
+else 
+    echo "you are not on master, deploying preview."
+    cd public
+    netlify deploy
+fi
