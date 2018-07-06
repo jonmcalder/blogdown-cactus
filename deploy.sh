9a82bf6588d2f8697be14cc0dfe63d72ef82c58d
lorenzwalthert <lorenz.walthert@icloud.com>
2018-07-06 18:20:53 +0200

    use env var

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
 echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
-if [[ "TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
+if [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
 then
 	  echo "you are on master, deploying production."
 	  netlify deploy -t $NETLIFYKEY
